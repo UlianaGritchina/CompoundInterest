12f9f954762aa8116bbf85f42e9fbed58dd4959e
Zhorobekov <zhorobekov25@icloud.com>
2022-01-22 01:28:10 +0600

    view controller commit

--- CompoundInterest/ViewController.swift
+++ CompoundInterest/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var firstDepositTF: UITextField!
     @IBOutlet weak var frequencyDepositTF: UITextField!
-    @IBOutlet weak var depostiTimeTF: UITextField!
-
+    @IBOutlet weak var depositTimeTF: UITextField!
     @IBOutlet weak var percentTF: UITextField!
     
     @IBOutlet weak var frequencyDepositLabel: UILabel!
@@ -28,16 +27,20 @@ class ViewController: UIViewController {
     
     @IBOutlet var textFieldCollection: [UITextField]!
     
+    private var currentTextField = UITextField()
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        calculateButton.isEnabled = false
         settingsStackView.layer.cornerRadius = 14
+        percentTF.rightView = UIView()
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
-        segmentControl.isHidden = false
-        userInterfaceIsOn()
-    }
+
 
     
     @IBAction func settingsTapped(_ sender: Any) {
@@ -47,6 +50,24 @@ class ViewController: UIViewController {
     
     @IBAction func segmentControlValueChanged(_ sender: Any) {
         segmentControl.selectedSegmentIndex == 0 ? (frequencyDepositLabel.text = "Ежемесячный депозит") : (frequencyDepositLabel.text = "Ежегодный депозит")
+    }
+    
+    
+    
+    private func calculateButtonIsOn() {
+        
+        var textfieldsIsEmpty = true
+        
+        for textField in textFieldCollection {
+            if textField.hasText {
+            textfieldsIsEmpty = false
+            } else {
+            textfieldsIsEmpty = true
+            }
+        }
+        
+        calculateButton.isEnabled = textfieldsIsEmpty ? false : true
+        
     }
     
     private func userInterfaceIsOn() {
@@ -60,6 +81,91 @@ class ViewController: UIViewController {
         settingsStackView.isHidden = userInterfaceIsOn
         segmentControl.isHidden = userInterfaceIsOn
         calculateButton.isEnabled = userInterfaceIsOn
+    }
+    
+    @objc private func didTapDone() {
+        calculateButtonIsOn()
+        view.endEditing(true)
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+        segmentControl.isHidden = false
+        userInterfaceIsOn()
+        calculateButtonIsOn()
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        currentTextField = textField
+        return true
+    }
+    
+    @objc private func nextTF() {
+        
+        switch currentTextField {
+        case firstDepositTF:
+            frequencyDepositTF.becomeFirstResponder()
+        case frequencyDepositTF:
+            depositTimeTF.becomeFirstResponder()
+        case depositTimeTF:
+            percentTF.becomeFirstResponder()
+        default:
+            firstDepositTF.becomeFirstResponder()
+        }
+    }
+    
+    @objc private func previousTF() {
+        
+        switch currentTextField {
+        case firstDepositTF:
+            percentTF.becomeFirstResponder()
+        case frequencyDepositTF:
+            firstDepositTF.becomeFirstResponder()
+        case depositTimeTF:
+            frequencyDepositTF.becomeFirstResponder()
+        default:
+            firstDepositTF.becomeFirstResponder()
+        }
+    }
+    
+     func textFieldDidBeginEditing(_ textField: UITextField) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(didTapDone)
+        )
+         
+        let nextButton = UIBarButtonItem(
+            image: UIImage(systemName: "arrow.down"),
+            style: .done,
+            target: self,
+            action: #selector(nextTF)
+        )
+        
+         let previousButton = UIBarButtonItem(
+             image: UIImage(systemName: "arrow.up"),
+             style: .done,
+             target: self,
+             action: #selector(previousTF)
+         )
+         
+         
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [previousButton, nextButton, flexBarButton, doneButton]
     }
     
 }
