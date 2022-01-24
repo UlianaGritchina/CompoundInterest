5da49d085ce45f7b39cb318b9648b46df1a3f325
Zhorobekov <zhorobekov25@icloud.com>
2022-01-24 17:33:29 +0600

    add view controllers

---
+++ CompoundInterest/ViewControllers/MoreResultTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MoreResultTableViewController.swift
+//  CompoundInterest
+//
+//  Created by Эрмек Жоробеков on 24.01.2022.
+//
+
+import UIKit
+
+class MoreResultTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
---
+++ CompoundInterest/ViewControllers/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  CompoundInterest
+//
+//  Created by Эрмек Жоробеков on 24.01.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var allDeposits: UILabel!
+    @IBOutlet weak var percentProfit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
