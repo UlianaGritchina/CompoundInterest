27d7a816f6a225257c19d499b32cc2788dccba04
UlianaGritchina <ugritchina@icloud.com>
2022-01-25 09:19:42 +0300

    Added model, getResults, display results.

--- CompoundInterest/ViewControllers/ResultViewController.swift
+++ CompoundInterest/ViewControllers/ResultViewController.swift
@@ -13,10 +13,13 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var allDeposits: UILabel!
     @IBOutlet weak var percentProfit: UILabel!
     
+    var resultModel: Results!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        resultLabel.text = String(resultModel.mainResalt)
+        allDeposits.text = String(resultModel.sum)
+        percentProfit.text = String(resultModel.proc)
     }
-    
 }
